fedafe8d84b693c6e4a405ec6a32fe19580b2ba0
monellz <monell.zhong@gmail.com>
2020-11-26 15:14:33 +0800

    Modify it to a safer script

--- sync_before_commit.sh
+++ sync_before_commit.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 set -x
 
+if [[ -n "$1" ]] && [[ "$1" != "rev" ]]; then
+    echo "Wrong Parameter: $1"
+    exit 1
+fi
 
 if [ "$1" == "rev" ]; then
     rsync -a --delete --info=progress2 libgomp/ gcc-8.3.0/libgomp
